2bd97f318d7c46f691ff79c11e0ea19da9c4d2a3
Katie <kaytay418@live.com>
2022-07-01 14:06:15 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,21 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        This project was coded by{" "}
-        <a href="https://www.katie-latham.com" rel="noreferrer" target="_blank">
-          Katie Latham
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Kaytay418/react-weather-app"
-          rel="noreferrer"
-          target="_blank"
-        >
-          open-sourced
-        </a>{" "}
-        on Github
-      </footer>
+      <div className="container">
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.katie-latham.com"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Katie Latham
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Kaytay418/react-weather-app"
+            rel="noreferrer"
+            target="_blank"
+          >
+            open-sourced
+          </a>{" "}
+          on Github
+        </footer>
+      </div>
     </div>
   );
 }
