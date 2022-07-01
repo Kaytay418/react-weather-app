7e947a6c90de0fac211f409f81da6fdf2a710e87
Katie <kaytay418@live.com>
2022-07-01 13:43:59 -0700

    Added Github and personal URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,23 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather Application</div>;
+  return (
+    <div className="App">
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.katie-latham.com" rel="noreferrer" target="_blank">
+          Katie Latham
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Kaytay418/react-weather-app"
+          rel="noreferrer"
+          target="_blank"
+        >
+          open-sourced
+        </a>{" "}
+        on Github
+      </footer>
+    </div>
+  );
 }
